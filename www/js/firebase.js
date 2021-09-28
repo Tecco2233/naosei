3e68d4b32aacd358bb3a5af54a10218b8364a476
Gabriel L. Tecco <51713625+Tecco2233@users.noreply.github.com>
2021-09-28 14:45:51 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAbqFZB1Yl3E1aPfSFR3-sZ4XFiH0R7v6E",
+  authDomain: "bancopam-521b0.firebaseapp.com",
+  projectId: "bancopam-521b0",
+  storageBucket: "bancopam-521b0.appspot.com",
+  messagingSenderId: "412079380493",
+  appId: "1:412079380493:web:581a15167cfe2b1f111d44",
+  measurementId: "G-TLN289Y4M7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
